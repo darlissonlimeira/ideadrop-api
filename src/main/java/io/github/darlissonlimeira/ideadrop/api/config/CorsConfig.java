1969402656bd01ea4d37ee01e9d2f4f7e8ce852c
Darlisson Limeira <darlisson.dev@gmail.com>
2026-01-10 15:19:44 -0300

    fix(CorsConfig.java): remove unnecessary print

--- src/main/java/io/github/darlissonlimeira/ideadrop/api/config/CorsConfig.java
+++ src/main/java/io/github/darlissonlimeira/ideadrop/api/config/CorsConfig.java
@@ -1,23 +1,15 @@
 package io.github.darlissonlimeira.ideadrop.api.config;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${app.cors.allowed-origins}")
     private String[] allowedOrigins;
-
-    @PostConstruct
-    public void init() {
-        System.out.println(Arrays.toString(allowedOrigins));
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
